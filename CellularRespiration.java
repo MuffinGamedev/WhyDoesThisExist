86c9335c2d94b522ef4170eac01d56276fb29ca5
LazrElectric <33110037+LazrElectric@users.noreply.github.com>
2017-11-29 08:40:39 -0500

    Update CellularRespiration.java

--- CellularRespiration.java
+++ CellularRespiration.java
@@ -1,2 +1,16 @@
 public class CellularRespiration {
+    private int glucose;
+    private int ATP; 
+    private int ADP; 
+    private int NAD;
+    private int NADH;
+    private int H2O;
+    private int CO2;
+    private int coenzymeA;
+    private int FADH2;
+    private int FAD; 
+    private int O2;
+    
+    public static void glycolysis() {
+    }
 }
